06b34bbc310881143a50a32f7ca3649c9c496b9d
Martin Kuba <mkuba@student.umb.sk>
2023-02-11 12:01:35 +0100

    First version

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -6,229 +6,117 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  desiatkova = 0;
-  input = 0;
-  binArray = [0 ,0 ,0 ,0 ,0 ,0 ,0, 0]
-  toBin(cislo: string) : void {
-    let convert = parseInt(cislo);
-    let binary = convert.toString(2);
-    console.log(binary.length)
-    let temp = "";
-    for (let i = binary.length; i <= 7; i++) {
-      temp = temp.concat("0");
-    }
-    let vysledok = temp.concat(binary);
-    if(vysledok.charAt(7) == "1"){
-      const checkbox = document.getElementById(
-        '1',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
+  something = 0;
+  shown1 = false;
+  shown2 = false;
+  shown3 = false;
+  shown4 = false;
+  shown5 = false;
+  shown6 = false;
+  shown7 = false;
+  shown8 = false;
+  decArray = [0 ,0 ,0 ,0 ,0 ,0 ,0, 0];
+  toBin(cislo : string) : void {
+    this.decArray = [0 ,0 ,0 ,0 ,0 ,0 ,0, 0];
+    let binArray = [0, 0, 0, 0, 0, 0, 0, 0]
+    let changed = Number(cislo);
+    let number = changed.toString(2);
+    let i = number.length;
+    let index = 7;
+    while (i > 0) {
+      if (number.charAt(i - 1) == "1") {
+        binArray[index] = 1;
       }
+      index--;
+      i--;
     }
-    else if(vysledok.charAt(7) == "0"){
-      const checkbox = document.getElementById(
-        '1',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-
-      }
-    }
-    if(vysledok.charAt(6) == "1"){
-      const checkbox = document.getElementById(
-        '2',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(6) == "0"){
-      const checkbox = document.getElementById(
-        '2',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-    if(vysledok.charAt(5) == "1"){
-      const checkbox = document.getElementById(
-        '3',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(5) == "0"){
-      const checkbox = document.getElementById(
-        '3',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-    if(vysledok.charAt(4) == "1"){
-      const checkbox = document.getElementById(
-        '4',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(4) == "0"){
-      const checkbox = document.getElementById(
-        '4',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-    if(vysledok.charAt(3) == "1"){
-      const checkbox = document.getElementById(
-        '5',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(3) == "0"){
-      const checkbox = document.getElementById(
-        '5',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-    if(vysledok.charAt(2) == "1"){
-      const checkbox = document.getElementById(
-        '6',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(2) == "0"){
-      const checkbox = document.getElementById(
-        '6',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-    if(vysledok.charAt(1) == "1"){
-      const checkbox = document.getElementById(
-        '7',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(1) == "0"){
-      const checkbox = document.getElementById(
-        '7',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-
-    if(vysledok.charAt(0) == "1"){
-      const checkbox = document.getElementById(
-        '8',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = true;
-      }
-    }
-    else if(vysledok.charAt(0) == "0"){
-      const checkbox = document.getElementById(
-        '8',
-      ) as HTMLInputElement | null;
-      if (checkbox != null) {
-        checkbox.checked = false;
-      }
-    }
-
-
+    this.shown8 = binArray[7] == 1;
+    this.shown7 = binArray[6] == 1;
+    this.shown6 = binArray[5] == 1;
+    this.shown5 = binArray[4] == 1;
+    this.shown4 = binArray[3] == 1;
+    this.shown3 = binArray[2] == 1;
+    this.shown2 = binArray[1] == 1;
+    this.shown1 = binArray[0] == 1;
   }
-  toDecimal(cislo: number) : void {
-    let vysledok: number;
-    if(cislo == 1){
-      if(this.binArray[7] == 1) {
-        this.binArray[7] = 0;
-      }
-      else{
-        this.binArray[7] = 1;
-      }
-
+  toDecimal(cislo : number) : void {
+    switch (cislo){
+      case 8:
+        if(this.decArray[0] == 0){
+          this.decArray[0] = 1;
+        }
+        else{
+          this.decArray[0] = 0;
+          this.shown1 = false;
+        }
+        break
+      case 7:
+        if(this.decArray[1] == 0){
+          this.decArray[1] = 1;
+        }
+        else{
+          this.decArray[1] = 0;
+          this.shown2 = false;
+        }
+        break
+      case 6:
+        if(this.decArray[2] == 0){
+          this.decArray[2] = 1;
+        }
+        else{
+          this.decArray[2] = 0;
+          this.shown3 = false;
+        }
+        break
+      case 5:
+        if(this.decArray[3] == 0){
+          this.decArray[3] = 1;
+        }
+        else{
+          this.decArray[3] = 0;
+          this.shown4 = false;
+        }
+        break
+      case 4:
+        if(this.decArray[4] == 0){
+          this.decArray[4] = 1;
+        }
+        else{
+          this.decArray[4] = 0;
+          this.shown5 = false;
+        }
+        break
+      case 3:
+        if(this.decArray[5] == 0){
+          this.decArray[5] = 1;
+        }
+        else{
+          this.decArray[5] = 0;
+          this.shown6 = false;
+        }
+        break
+      case 2:
+        if(this.decArray[6] == 0){
+          this.decArray[6] = 1;
+        }
+        else{
+          this.decArray[6] = 0;
+          this.shown7 = false;
+        }
+        break
+      case 1:
+        if(this.decArray[7] == 0){
+          this.decArray[7] = 1;
+        }
+        else{
+          this.decArray[7] = 0;
+          this.shown8 = false;
+        }
+        break
     }
-    if(cislo == 2){
-      if(this.binArray[6] == 1) {
-        this.binArray[6] = 0;
-      }
-      else{
-        this.binArray[6] = 1;
-      }
-
-    }
-    if(cislo == 3){
-      if(this.binArray[5] == 1) {
-        this.binArray[5] = 0;
-      }
-      else{
-        this.binArray[5] = 1;
-      }
-
-    }
-    if(cislo == 4){
-      if(this.binArray[4] == 1) {
-        this.binArray[4] = 0;
-      }
-      else{
-        this.binArray[4] = 1;
-      }
-
-    }
-    if(cislo == 5){
-      if(this.binArray[3] == 1) {
-        this.binArray[3] = 0;
-      }
-      else{
-        this.binArray[3] = 1;
-      }
-
-    }
-    if(cislo == 6){
-      if(this.binArray[2] == 1) {
-        this.binArray[2] = 0;
-      }
-      else{
-        this.binArray[2] = 1;
-      }
-
-    }
-    if(cislo == 7){
-      if(this.binArray[1] == 1) {
-        this.binArray[1] = 0;
-      }
-      else{
-        this.binArray[1] = 1;
-      }
-
-    }
-    if(cislo == 8){
-      if(this.binArray[0] == 1) {
-        this.binArray[0] = 0;
-      }
-      else{
-        this.binArray[0] = 1;
-      }
-
-    }
-    this.binArray.toString();
-    let joined = this.binArray.join("");
-    vysledok = parseInt(joined,2)
-    console.log(vysledok);
-    this.input = vysledok;
+    let prevod = this.decArray.join("");
+    prevod.toString();
+    this.something = parseInt(prevod,2);
   }
+
 }
