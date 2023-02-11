1dcca159016316cef99f1cadae289a476734de45
Martin Kuba <mkuba@student.umb.sk>
2023-02-11 13:52:02 +0100

    Final version reworked

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,43 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: `app.component.html`,
+  styles: []
 })
 export class AppComponent {
-  something = 0;
-  shown1 = false;
-  shown2 = false;
-  shown3 = false;
-  shown4 = false;
-  shown5 = false;
-  shown6 = false;
-  shown7 = false;
-  shown8 = false;
-  toBin(cislo : string) : void {
-    let binArray = [0, 0, 0, 0, 0, 0, 0, 0]
-    let changed = Number(cislo);
-    let number = changed.toString(2);
-    let i = number.length;
-    let index = 7;
-    while (i > 0) {
-      if (number.charAt(i - 1) == "1") {
-        binArray[index] = 1;
-      }
-      index--;
-      i--;
-    }
-    this.shown8 = binArray[7] == 1;
-    this.shown7 = binArray[6] == 1;
-    this.shown6 = binArray[5] == 1;
-    this.shown5 = binArray[4] == 1;
-    this.shown4 = binArray[3] == 1;
-    this.shown3 = binArray[2] == 1;
-    this.shown2 = binArray[1] == 1;
-    this.shown1 = binArray[0] == 1;
-    let decimal = binArray.toString();
-    this.something = parseInt(decimal,2);
+  decimalValue = 0;
+  binaryArray = [false, false, false, false, false, false, false, false];
+
+  updateBinary() {
+    const binary = this.decimalValue.toString(2).padStart(8, '0');
+    this.binaryArray = binary.split('').map(digit => digit === '1');
   }
 
+  updateDecimal() {
+    const binary = this.binaryArray.map(checked => checked ? '1' : '0').join('');
+    this.decimalValue = parseInt(binary, 2);
+  }
 }
