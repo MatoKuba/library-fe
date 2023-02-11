ec37326c5bbe5655a1dc32d87c3941b850e22c64
Martin Kuba <mkuba@student.umb.sk>
2023-02-11 12:44:11 +0100

    Third Version

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -15,9 +15,7 @@ export class AppComponent {
   shown6 = false;
   shown7 = false;
   shown8 = false;
-  decArray = [0 ,0 ,0 ,0 ,0 ,0 ,0, 0];
   toBin(cislo : string) : void {
-    this.decArray = [0 ,0 ,0 ,0 ,0 ,0 ,0, 0];
     let binArray = [0, 0, 0, 0, 0, 0, 0, 0]
     let changed = Number(cislo);
     let number = changed.toString(2);
@@ -38,85 +36,8 @@ export class AppComponent {
     this.shown3 = binArray[2] == 1;
     this.shown2 = binArray[1] == 1;
     this.shown1 = binArray[0] == 1;
-  }
-  toDecimal(cislo : number) : void {
-    switch (cislo){
-      case 8:
-        if(this.decArray[0] == 0){
-          this.decArray[0] = 1;
-        }
-        else{
-          this.decArray[0] = 0;
-          this.shown1 = false;
-        }
-        break
-      case 7:
-        if(this.decArray[1] == 0){
-          this.decArray[1] = 1;
-        }
-        else{
-          this.decArray[1] = 0;
-          this.shown2 = false;
-        }
-        break
-      case 6:
-        if(this.decArray[2] == 0){
-          this.decArray[2] = 1;
-        }
-        else{
-          this.decArray[2] = 0;
-          this.shown3 = false;
-        }
-        break
-      case 5:
-        if(this.decArray[3] == 0){
-          this.decArray[3] = 1;
-        }
-        else{
-          this.decArray[3] = 0;
-          this.shown4 = false;
-        }
-        break
-      case 4:
-        if(this.decArray[4] == 0){
-          this.decArray[4] = 1;
-        }
-        else{
-          this.decArray[4] = 0;
-          this.shown5 = false;
-        }
-        break
-      case 3:
-        if(this.decArray[5] == 0){
-          this.decArray[5] = 1;
-        }
-        else{
-          this.decArray[5] = 0;
-          this.shown6 = false;
-        }
-        break
-      case 2:
-        if(this.decArray[6] == 0){
-          this.decArray[6] = 1;
-        }
-        else{
-          this.decArray[6] = 0;
-          this.shown7 = false;
-        }
-        break
-      case 1:
-        if(this.decArray[7] == 0){
-          this.decArray[7] = 1;
-        }
-        else{
-          this.decArray[7] = 0;
-          this.shown8 = false;
-        }
-        break
-    }
-    let prevod = this.decArray.join("");
-    prevod.toString();
-    this.something = parseInt(prevod,2);
+    let decimal = binArray.toString();
+    this.something = parseInt(decimal,2);
   }
 
 }
