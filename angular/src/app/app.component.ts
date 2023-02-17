d7b10ebbd61ed5aa9f71ce46038207221efafd46
Kuba <mkuba@student.umb.sk>
2023-02-17 19:31:27 +0100

    kuba-uloha2

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,21 +1,105 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+interface User {
+  id: string;
+  name: string;
+  contact: string;
+}
+
+interface Book {
+  id: string;
+  name: string;
+  author: string;
+  available: boolean;
+}
+
+interface Borrowing {
+  id: string;
+  bookId: string;
+  userId: string;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: `app.component.html`,
-  styles: []
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  decimalValue = 0;
-  binaryArray = [false, false, false, false, false, false, false, false];
+  showUser = false;
+  showBook = false;
+  showBorrowing = false;
 
-  updateBinary() {
-    const binary = this.decimalValue.toString(2).padStart(8, '0');
-    this.binaryArray = binary.split('').map(digit => digit === '1');
+  userId = '';
+  userName = '';
+  userContact = '';
+  users: User[] = [];
+
+  bookId = '';
+  bookName = '';
+  bookAuthor = '';
+  bookAvailable = true;
+  books: Book[] = [];
+
+  borrowingId = '';
+  borrowingBook = '';
+  borrowingUser = '';
+  borrowings: Borrowing[] = [];
+
+  showUsers(): void {
+    this.showUser = true;
+    this.showBook = false;
+    this.showBorrowing = false;
   }
 
-  updateDecimal() {
-    const binary = this.binaryArray.map(checked => checked ? '1' : '0').join('');
-    this.decimalValue = parseInt(binary, 2);
+  showBooks(): void {
+    this.showUser = false;
+    this.showBook = true;
+    this.showBorrowing = false;
+  }
+
+  showBorrowings(): void {
+    this.showUser = false;
+    this.showBook = false;
+    this.showBorrowing = true;
+  }
+
+  addUser(): void {
+    const user: User = {
+      id: this.userId,
+      name: this.userName,
+      contact: this.userContact
+    };
+    this.users.push(user);
+
+    this.userId = '';
+    this.userName = '';
+    this.userContact = '';
+  }
+
+  addBook(): void {
+    const book: Book = {
+      id: this.bookId,
+      name: this.bookName,
+      author: this.bookAuthor,
+      available: this.bookAvailable
+    };
+    this.books.push(book);
+
+    this.bookId = '';
+    this.bookName = '';
+    this.bookAuthor = '';
+    this.bookAvailable = true;
+  }
+
+  addBorrowing(): void {
+    const borrowing: Borrowing = {
+      id: this.borrowingId,
+      bookId: this.borrowingBook,
+      userId: this.borrowingUser
+    };
+    this.borrowings.push(borrowing);
+
+    this.borrowingId = '';
+    this.borrowingBook = '';
+    this.borrowingUser = '';
   }
 }
